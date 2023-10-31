528b73b9faa1914184bbf53bbcfa92f825e846f9
Mujahed Ali <mujahed97@gmail.com>
2023-10-31 16:28:15 -0400

    finished_card

--- card.rb
+++ card.rb
@@ -1,6 +1,30 @@
 class Card
-    def initialize
+    # @@face_value = ("A".."Z").to_a
 
+    # a = Class.new('A')
+    # @value = 'A'
+    # @face_up = 'A'
+    attr_reader :face_down, :value
+    def initialize (value)
+        @face_value = value
+        @face_down = true 
     end
 
+    def reveal
+        @face_down = false
+    end
+
+    def hide
+        @face_down = true
+    end
+
+    def to_s
+        if self.face_down == false
+            return @face_value.to_s
+        end
+    end
+
+    def ==(card2)
+        return self.face_value == card.face_value
+    end
 end
